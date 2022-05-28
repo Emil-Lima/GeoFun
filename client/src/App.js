cdacc502ff93e45ff737eed0ea2fc321b5ec4a01
mattnorris1990 <mattnorris1990@gmail.com>
2022-05-28 11:48:15 +0100

    fetch countries data working on initial load useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import AppContainer from './containers/AppContainer';
 
 
-
 function App() {
+
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    fetchCountries()
+  }, [])
+
+  const fetchCountries = () => {
+
+    fetch("https://restcountries.com/v3.1/region/europe")
+    .then((res) => res.json())
+    .then((countries) => setCountries(countries))
+    
+}
+
   return (
     <>
-      <AppContainer/>
+      <AppContainer countries = {countries}/>
       
     </>
     
