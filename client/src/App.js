39061f18a75d4bf095a67b4f0df0ec07a5e411c5
mattnorris1990 <mattnorris1990@gmail.com>
2022-05-28 11:30:04 +0100

    loaded map in map container

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
+import './App.css';
 import React, {useState} from 'react';
 import AppContainer from './containers/AppContainer';
+
 
 
 function App() {
   return (
     <>
       <AppContainer/>
+      
     </>
     
   )
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import { MapContainer, TileLayer, Popup, GeoJSON } from 'react-leaflet'
+import europeData from "../data/europe-info.json"
 
 const Map = () => {
   return (
-    <div>
-      <h2>This is the map zone</h2>
-    </div>
+    <>
+      <h1>Map testing</h1>
+
+      <MapContainer center={[52.015670, 18.635115]} zoom={4} scrollWheelZoom={false} className="MapContainer">
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      
+      {
+        europeData.features.map((country, index) => {
+
+            return (
+            <GeoJSON attribution="&copy; credits due..." data={country} key={index}>
+              <Popup>
+                <h3>{country.properties.name}</h3>
+              </Popup>
+            </GeoJSON>)
+
+        })
+      }
+
+
+      </MapContainer>
+
+  </>
   )
 }
 
