63b4dba916b6d917fbb01d0ceb024aa9723066b5
mattnorris1990 <mattnorris1990@gmail.com>
2022-05-28 17:01:22 +0100

    added more to CountryDetails return

--- client/src/components/CountryDetails.js
+++ client/src/components/CountryDetails.js
@@ -8,6 +8,10 @@ const CountryDetails = ({selectedCountry, countries}) => {
   
   const countryCode = countryData[0].cca3
 
+  
+  const countryLanguages = Object.values(countryData[0].languages)
+  const languageList = countryLanguages.toString()
+
 
   return (
     <div>
@@ -16,7 +20,9 @@ const CountryDetails = ({selectedCountry, countries}) => {
       <img src={countryData[0].flags.png} />
       <p>Capital: {countryData[0].capital}</p>
       <p>Population: {countryData[0].population}</p>
-
+      {countryData[0].landlocked == true ? <p>Landlocked</p> : <p>Coastal</p>}
+      <p>People in {countryData[0].name.common} drive on the {countryData[0].car.side} side of the road </p>
+      <p>National Languages: {languageList}</p>
     </div>
   )
 }
