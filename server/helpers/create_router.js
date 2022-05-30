37ef945897ba4ce7567744c22e168b851b32fb5a
Derek Leafgreen <drl21788@gmail.com>
2022-05-30 13:29:57 +0100

    Format change.
    Co-authored-by: Emilio J. R. Lima <J-Rozas@users.noreply.github.com>

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -3,15 +3,15 @@ const ObjectID = require('mongodb').ObjectID;
 
 const appRouter = function (collection) {
 
-  const router = express.Router();
+    const router = express.Router();
 
-  router.get('/', (request, response) => {
-    collection.find()
-    .toArray()
-    .then(documents => response.json(documents))
-  });
-  
-  return router;
+    router.get('/', (request, response) => {
+        collection.find()
+            .toArray()
+            .then(documents => response.json(documents))
+    });
+
+    return router;
 
 };
 
