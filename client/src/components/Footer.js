153df122d708b286b4b7f8b50690c2b9c5ccbe31
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-05-31 11:04:09 +0100

    Include dev data in the footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,15 +1,34 @@
 import React from "react";
 import styled from "styled-components";
+import github_logo from "../assets/github_logo.png"
 
 const DevsContainer = styled.footer`
   background-color: red;
+  height: 10vh;
+`
+
+const Logo = styled.img`
+  width: 10vh;
 `
 
 const Footer = () => {
   return (
     <DevsContainer>
       <div>
-        <p>test</p>
+        <Logo src={github_logo}/>
+        <a href="https://github.com/mattnorris1990">Matt</a>
+      </div>
+      <div>
+        <Logo src={github_logo}/>
+        <a href="https://github.com/JazzRose">Jazz</a>
+      </div>
+      <div>
+        <Logo src={github_logo}/>
+        <a href="https://github.com/drleafgreen88">Derek</a>
+      </div>
+      <div>
+        <Logo src={github_logo}/>
+        <a href="https://github.com/J-Rozas">Emilio</a>
       </div>
     </DevsContainer>
   )
