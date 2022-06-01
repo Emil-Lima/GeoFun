40366aff96984476b2b03fefaeb94f2ee3252c29
Jasmine Mills <jazzrmills@gmail.com>
2022-06-01 11:52:56 +0100

    styling country select

--- client/src/components/CountryDetails.js
+++ client/src/components/CountryDetails.js
@@ -13,11 +13,11 @@ const CountryDetails = ({selectedCountry, countries}) => {
       <h1>{selectedCountry.name.common}</h1>
       {/* <h2>Native Name: {selectedCountry.name.nativeName[countryCode].common}</h2>    <<<< THIS DOESNT WORK BECAUSE THE CCA3 DOESNT ALWAYS MATCH */}
       <img src={selectedCountry.flags.png} />
-      <p>Capital: {selectedCountry.capital}</p>
-      <p>Population: {selectedCountry.population}</p>
-      {selectedCountry.landlocked == true ? <p>Landlocked</p> : <p>Coastal</p>}
-      <p>People in {selectedCountry.name.common} drive on the {selectedCountry.car.side} side of the road </p>
-      <p>National Languages: {languageList}</p>
+      <p><b>Capital:</b> {selectedCountry.capital}</p>
+      <p><b>Population:</b> {selectedCountry.population}</p>
+      <p><b>National Languages: </b>{languageList}</p>
+      {selectedCountry.landlocked == true ? <p><b>Landlocked</b></p> : <p><b>Coastal</b></p>}
+      <p>People in {selectedCountry.name.common} drive on the {selectedCountry.car.side}.</p>
     </div>
   )
 }
--- client/src/containers/SelectCountry.js
+++ client/src/containers/SelectCountry.js
@@ -1,7 +1,31 @@
 import React, { useState } from "react";
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import europeData from "../data/europe-info.json"
-import Header from "../components/Header";
+import Header from "../components/Header"; 
+import styled from "styled-components";
+
+const Button = styled.button`
+
+width:300px;
+font-size:30px;
+font-family:Verdana, Geneva, Tahoma, sans-serif;
+margin:10px;
+background-color: #00af2d;
+color:white;
+border:none;
+    &:hover{
+        text-decoration:underline;
+        transform:translateY(4px);
+        transition-duration: 0.2s;
+    }
+`
+const PlayGame = styled.div`
+display: flex;
+flex-direction: row;
+width:50%;
+margin:auto;
+justify-content: space-between;
+`
 
 const SelectCountry = ({profile, onHomeClick}) => {
 
@@ -45,7 +69,12 @@ const SelectCountry = ({profile, onHomeClick}) => {
   return (
     <div>
     <Header profile={profile} onHomeClick = {onHomeClick}></Header>
+    <PlayGame>
     <h2>Guess the country!</h2>
+    <Button onClick={playGame}>Play game</Button>
+    </PlayGame>
+    {mysteryCountry === null ? null : <p>Find {mysteryCountry} in the map!</p>}
+    {isCorrectAnswer === null ? null : isCorrectAnswer === true ? <p>Correct, you have found {mysteryCountry}!</p> : <p>Nope, that is {userSelection}</p>}
     <MapContainer center={[52.015670, 18.635115]} zoom={4} scrollWheelZoom={false} className="MapContainer" >
       <TileLayer
        attribution='&copy; <a href="Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
@@ -63,9 +92,6 @@ const SelectCountry = ({profile, onHomeClick}) => {
         })
       }
     </MapContainer>
-    <button onClick={playGame}>Play game</button>
-    {mysteryCountry === null ? null : <p>Find {mysteryCountry} in the map!</p>}
-    {isCorrectAnswer === null ? null : isCorrectAnswer === true ? <p>Correct, you have found {mysteryCountry}!</p> : <p>Nope, that is {userSelection}</p>}
     </div>
   )
 }
