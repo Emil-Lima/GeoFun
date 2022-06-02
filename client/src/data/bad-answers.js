3ceaa35067c46f98f6698190be6e40dea286dc96
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-02 08:35:34 +0100

    Fix grammar of good and bad answers

--- client/src/data/bad-answers.js
+++ client/src/data/bad-answers.js
@@ -1,5 +1,5 @@
 const getBadAnswers = (name, userAnswer, correctAnswer) => {
-  return [`❌ Incorrect! That was ${userAnswer}, not ${correctAnswer}. Don't you learn anything in school or what? 🧐`, `❌ No. Just no.`, `❌ Matt Norris`, `❌ You == smart returns false. That was ${userAnswer}.`, `❌ Andddd ${name} fails... again.`, `❌ Ok not ok 😐`, `❌ You are wrong, you absolute Anonymous Wombat. That is ${userAnswer}.`, `❌ Nope. *Note: You are supposed to select the right country. 🙄`, `❌ You are good at selecting the wrong country 🙂`, `❌ You tried... And you failed; that was ${userAnswer}.`]
+  return [`❌ Incorrect! This is ${userAnswer}, not ${correctAnswer}. Don't you learn anything in school or what? 🧐`, `❌ No. Just no.`, `❌ Matt Norris`, `❌ You == smart returns false. This is ${userAnswer}.`, `❌ Andddd ${name} fails... again.`, `❌ Ok not ok 😐`, `❌ You are wrong, you absolute Anonymous Wombat. This is ${userAnswer}.`, `❌ Nope. *Note: You are supposed to select the right country. 🙄`, `❌ You are good at selecting the wrong country 🙂`, `❌ You tried... And you failed; this is ${userAnswer}.`]
 }
 
 export default getBadAnswers;
