55195379cb982684eb78bb91f9efb90885774ba2
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-03 02:33:37 +0100

    Refactor hard-coded data outside of the component

--- client/src/containers/QuizContainer.js
+++ client/src/containers/QuizContainer.js
@@ -31,10 +31,12 @@ const Button = styled.button`
     transition-duration: 0.2s;
   }
 `;
+
 const Score = styled.h2`
   color: #076d5d;
   font-size: 30px;
 `;
+
 const ButtonFlex = styled.div`
   display: flex;
   flex-direction: column;
@@ -42,199 +44,199 @@ const ButtonFlex = styled.div`
   padding: 30px;
 `;
 
-const QuizContainer = ({ profile, onHomeClick }) => {
-  const questions = [
-    {
-      questionText: "What is the capital of France?",
-      answerOptions: [
-        { answer: "Riga", isCorrect: false },
-        { answer: "London", isCorrect: false },
-        { answer: "Paris", isCorrect: true },
-        { answer: "Dublin", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "Name a country that drives on the left.",
-      answerOptions: [
-        { answer: "Poland", isCorrect: false },
-        { answer: "Croatia", isCorrect: false },
-        { answer: "United Kingdom", isCorrect: true },
-        { answer: "France", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "Which flag contains the colour blue?",
-      answerOptions: [
-        { answer: "Belgium", isCorrect: false },
-        { answer: "Ireland", isCorrect: false },
-        { answer: "Austria", isCorrect: false },
-        { answer: "Greece", isCorrect: true },
-      ],
-    },
-    {
-      questionText: "Which country has no coastline?",
-      answerOptions: [
-        { answer: "Spain", isCorrect: false },
-        { answer: "Bosnia and Herzegovinia", isCorrect: false },
-        { answer: "Slovakia", isCorrect: true },
-        { answer: "Slovenia", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "What is the capital of Croatia?",
-      answerOptions: [
-        { answer: "Zagreb", isCorrect: true },
-        { answer: "Madrid", isCorrect: false },
-        { answer: "Paris", isCorrect: false },
-        { answer: "Sofia", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "Name a country that drives on the right.",
-      answerOptions: [
-        { answer: "Ireland", isCorrect: false },
-        { answer: "Cyprus", isCorrect: false },
-        { answer: "Andorra", isCorrect: true },
-        { answer: "Malta", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "Which flag does not contain the colour yellow?",
-      answerOptions: [
-        { answer: "Armenia", isCorrect: false },
-        { answer: "Belgium", isCorrect: false },
-        { answer: "Romania", isCorrect: false },
-        { answer: "Estonia", isCorrect: true },
-      ],
-    },
-    {
-      questionText: "Which country has no land borders?",
-      answerOptions: [
-        { answer: "Netherlands", isCorrect: false },
-        { answer: "Germany", isCorrect: false },
-        { answer: "Finland", isCorrect: false },
-        { answer: "Iceland", isCorrect: true },
-      ],
-    },
-    {
-      questionText: "What is the capital of Portugal?",
-      answerOptions: [
-        { answer: "Stockholm", isCorrect: false },
-        { answer: "Berlin", isCorrect: false },
-        { answer: "Lisbon", isCorrect: true },
-        { answer: "Athens", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "What is the approximate population of Spain?",
-      answerOptions: [
-        { answer: "40,000,000", isCorrect: true },
-        { answer: "60,000,000", isCorrect: false },
-        { answer: "50,000,000", isCorrect: false },
-        { answer: "30,000,000", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "Which flag contains the colour red?",
-      answerOptions: [
-        { answer: "Belgium", isCorrect: false },
-        { answer: "Ireland", isCorrect: false },
-        { answer: "Iceland", isCorrect: true },
-        { answer: "Sweden", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "Which of these does not speak German?",
-      answerOptions: [
-        { answer: "Germany", isCorrect: false },
-        { answer: "Belgium", isCorrect: false },
-        { answer: "Luxembourg", isCorrect: false },
-        { answer: "Poland", isCorrect: true },
-      ],
-    },
-    {
-      questionText: "What is the capital of Lithuania?",
-      answerOptions: [
-        { answer: "Vilnius", isCorrect: true },
-        { answer: "Madrid", isCorrect: false },
-        { answer: "Paris", isCorrect: false },
-        { answer: "Sofia", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "Where is Skopje?",
-      answerOptions: [
-        { answer: "Iceland", isCorrect: false },
-        { answer: "Bulgaria", isCorrect: false },
-        { answer: "North Mecedonia", isCorrect: true },
-        { answer: "Estonia", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "Which flag has a cross?",
-      answerOptions: [
-        { answer: "Latvia", isCorrect: false },
-        { answer: "Russia", isCorrect: false },
-        { answer: "Austria", isCorrect: false },
-        { answer: "Switzerland", isCorrect: true },
-      ],
-    },
-    {
-      questionText: "Which country has the smallest population?",
-      answerOptions: [
-        { answer: "Licthenstein", isCorrect: true },
-        { answer: "Germany", isCorrect: false },
-        { answer: "Finland", isCorrect: false },
-        { answer: "Iceland", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "Which country has the largest population?",
-      answerOptions: [
-        { answer: "Italy", isCorrect: true },
-        { answer: "Spain", isCorrect: false },
-        { answer: "Ukraine", isCorrect: false },
-        { answer: "Poland", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "Where do the residents speak Dutch?",
-      answerOptions: [
-        { answer: "Ireland", isCorrect: false },
-        { answer: "Serbia", isCorrect: false },
-        { answer: "Netherlands", isCorrect: true },
-        { answer: "Denmark", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "Where is Warsaw?",
-      answerOptions: [
-        { answer: "Italy", isCorrect: false },
-        { answer: "Norway", isCorrect: false },
-        { answer: "Poland", isCorrect: true },
-        { answer: "France", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "Where is Brussels?",
-      answerOptions: [
-        { answer: "Portugal", isCorrect: false },
-        { answer: "Belgium", isCorrect: true },
-        { answer: "Bosnia and Herzegovina", isCorrect: false },
-        { answer: "Belarus", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "Where is Copenhagen?",
-      answerOptions: [
-        { answer: "Malta", isCorrect: false },
-        { answer: "Finland", isCorrect: false },
-        { answer: "Greece", isCorrect: false },
-        { answer: "Denmark", isCorrect: true },
-      ],
-    },
-  ];
+const questions = [
+  {
+    questionText: "What is the capital of France?",
+    answerOptions: [
+      { answer: "Riga", isCorrect: false },
+      { answer: "London", isCorrect: false },
+      { answer: "Paris", isCorrect: true },
+      { answer: "Dublin", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Name a country that drives on the left.",
+    answerOptions: [
+      { answer: "Poland", isCorrect: false },
+      { answer: "Croatia", isCorrect: false },
+      { answer: "United Kingdom", isCorrect: true },
+      { answer: "France", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Which flag contains the colour blue?",
+    answerOptions: [
+      { answer: "Belgium", isCorrect: false },
+      { answer: "Ireland", isCorrect: false },
+      { answer: "Austria", isCorrect: false },
+      { answer: "Greece", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "Which country has no coastline?",
+    answerOptions: [
+      { answer: "Spain", isCorrect: false },
+      { answer: "Bosnia and Herzegovinia", isCorrect: false },
+      { answer: "Slovakia", isCorrect: true },
+      { answer: "Slovenia", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What is the capital of Croatia?",
+    answerOptions: [
+      { answer: "Zagreb", isCorrect: true },
+      { answer: "Madrid", isCorrect: false },
+      { answer: "Paris", isCorrect: false },
+      { answer: "Sofia", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Name a country that drives on the right.",
+    answerOptions: [
+      { answer: "Ireland", isCorrect: false },
+      { answer: "Cyprus", isCorrect: false },
+      { answer: "Andorra", isCorrect: true },
+      { answer: "Malta", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Which flag does not contain the colour yellow?",
+    answerOptions: [
+      { answer: "Armenia", isCorrect: false },
+      { answer: "Belgium", isCorrect: false },
+      { answer: "Romania", isCorrect: false },
+      { answer: "Estonia", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "Which country has no land borders?",
+    answerOptions: [
+      { answer: "Netherlands", isCorrect: false },
+      { answer: "Germany", isCorrect: false },
+      { answer: "Finland", isCorrect: false },
+      { answer: "Iceland", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "What is the capital of Portugal?",
+    answerOptions: [
+      { answer: "Stockholm", isCorrect: false },
+      { answer: "Berlin", isCorrect: false },
+      { answer: "Lisbon", isCorrect: true },
+      { answer: "Athens", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What is the approximate population of Spain?",
+    answerOptions: [
+      { answer: "40,000,000", isCorrect: true },
+      { answer: "60,000,000", isCorrect: false },
+      { answer: "50,000,000", isCorrect: false },
+      { answer: "30,000,000", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Which flag contains the colour red?",
+    answerOptions: [
+      { answer: "Belgium", isCorrect: false },
+      { answer: "Ireland", isCorrect: false },
+      { answer: "Iceland", isCorrect: true },
+      { answer: "Sweden", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Which of these does not speak German?",
+    answerOptions: [
+      { answer: "Germany", isCorrect: false },
+      { answer: "Belgium", isCorrect: false },
+      { answer: "Luxembourg", isCorrect: false },
+      { answer: "Poland", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "What is the capital of Lithuania?",
+    answerOptions: [
+      { answer: "Vilnius", isCorrect: true },
+      { answer: "Madrid", isCorrect: false },
+      { answer: "Paris", isCorrect: false },
+      { answer: "Sofia", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Where is Skopje?",
+    answerOptions: [
+      { answer: "Iceland", isCorrect: false },
+      { answer: "Bulgaria", isCorrect: false },
+      { answer: "North Mecedonia", isCorrect: true },
+      { answer: "Estonia", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Which flag has a cross?",
+    answerOptions: [
+      { answer: "Latvia", isCorrect: false },
+      { answer: "Russia", isCorrect: false },
+      { answer: "Austria", isCorrect: false },
+      { answer: "Switzerland", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "Which country has the smallest population?",
+    answerOptions: [
+      { answer: "Licthenstein", isCorrect: true },
+      { answer: "Germany", isCorrect: false },
+      { answer: "Finland", isCorrect: false },
+      { answer: "Iceland", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Which country has the largest population?",
+    answerOptions: [
+      { answer: "Italy", isCorrect: true },
+      { answer: "Spain", isCorrect: false },
+      { answer: "Ukraine", isCorrect: false },
+      { answer: "Poland", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Where do the residents speak Dutch?",
+    answerOptions: [
+      { answer: "Ireland", isCorrect: false },
+      { answer: "Serbia", isCorrect: false },
+      { answer: "Netherlands", isCorrect: true },
+      { answer: "Denmark", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Where is Warsaw?",
+    answerOptions: [
+      { answer: "Italy", isCorrect: false },
+      { answer: "Norway", isCorrect: false },
+      { answer: "Poland", isCorrect: true },
+      { answer: "France", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Where is Brussels?",
+    answerOptions: [
+      { answer: "Portugal", isCorrect: false },
+      { answer: "Belgium", isCorrect: true },
+      { answer: "Bosnia and Herzegovina", isCorrect: false },
+      { answer: "Belarus", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Where is Copenhagen?",
+    answerOptions: [
+      { answer: "Malta", isCorrect: false },
+      { answer: "Finland", isCorrect: false },
+      { answer: "Greece", isCorrect: false },
+      { answer: "Denmark", isCorrect: true },
+    ],
+  },
+];
 
+const QuizContainer = ({ profile, onHomeClick }) => {
   const [question, setQuestion] = useState(0);
   const [score, setScore] = useState(0);
   const [gameStarted, setGameStarted] = useState(false);
