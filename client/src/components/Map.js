1a70153ca5af068fb9bfe5f1ddd45c5311dd57b9
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-04 13:46:02 +0100

    Delete redundant angle brackets

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -20,52 +20,50 @@ const Button = styled.button`
 
 const Map = ({ countries, addSavedCountry }) => {
   return (
-    <>
-      <MapContainer
-        center={[52.01567, 18.635115]}
-        zoom={4}
-        scrollWheelZoom={false}
-        className="MapContainer"
-      >
-        <TileLayer
-          attribution='&copy; <a href="Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
-          url="https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg"
-        />
+    <MapContainer
+      center={[52.01567, 18.635115]}
+      zoom={4}
+      scrollWheelZoom={false}
+      className="MapContainer"
+    >
+      <TileLayer
+        attribution='&copy; <a href="Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
+        url="https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg"
+      />
 
-        {europeData.features.map((country, index) => {
-          let currentData = "";
+      {europeData.features.map((country, index) => {
+        let currentData = "";
 
-          const countryCode = country.properties.iso_a3;
+        const countryCode = country.properties.iso_a3;
 
-          for (let cou of countries) {
-            if (cou.cca3 == countryCode) {
-              currentData = cou;
-            }
+        for (let cou of countries) {
+          if (cou.cca3 == countryCode) {
+            currentData = cou;
           }
+        }
 
-          const handleClick = () => {
-            addSavedCountry(currentData.cca3);
-          };
+        const handleClick = () => {
+          addSavedCountry(currentData.cca3);
+        };
 
-          return (
-            <GeoJSON
-              attribution="&copy; credits due..."
-              data={country}
-              key={index}
-            >
-              <Popup>
-                <h3>
-                  {country.properties.name} {currentData.flag}
-                </h3>
-                <h4>{currentData.subregion}</h4>
+        return (
+          <GeoJSON
+            attribution="&copy; credits due..."
+            data={country}
+            key={index}
+          >
+            <Popup>
+              <h3>
+                {country.properties.name} {currentData.flag}
+              </h3>
+              <h4>{currentData.subregion}</h4>
 
-                <Button onClick={handleClick}>Save</Button>
-              </Popup>
-            </GeoJSON>
-          );
-        })}
-      </MapContainer>
-    </>
+              <Button onClick={handleClick}>Save</Button>
+            </Popup>
+          </GeoJSON>
+        );
+      })}
+    </MapContainer>
   );
 };
 
